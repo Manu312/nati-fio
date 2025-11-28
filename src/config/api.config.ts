2186692fe035c0759912aa07fb295d4286adb03d
Emanuel Mercado <emercado@lnexternos.com.ar>
2025-11-28 15:44:13 +0000

    a

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -4,7 +4,7 @@
  */
 
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL,
   TIMEOUT: 30000, // 30 segundos
   RETRY_ATTEMPTS: 3,
 } as const;
